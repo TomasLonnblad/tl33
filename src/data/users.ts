e163260365b072bb9f00e0d2385e1940d3ee1ae8
TomasLonnblad <tomaslonnblad@gmail.com>
2025-12-31 11:10:11 +0100

    x

---
+++ src/data/users.ts
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    name: "Tomas Lönnblad",
+    username: "Aarhamkhnz",
+    email: "tomaslonnblad@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/43849669",
+    role: "administrator",
+  },
+  {
+    id: "2",
+    name: "Tomas Lönnblad",
+    username: "ammarkhnz",
+    email: "hello@ammarkhnz.com",
+    avatar: "",
+    role: "admin",
+  },
+];
+
+export const rootUser = users[0];
